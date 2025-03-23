b1a93be5afd1590b739534c85ff98c5248b78d92
Guilherme Nunes Trofino <guitrofino@gmail.com>
2025-03-23 17:05:25 +0100

    [*] displayManager.h format file

--- src/displayManager.h
+++ src/displayManager.h
@@ -1,10 +1,13 @@
 #ifndef MESSAGE_DISPLAY_H
 #define MESSAGE_DISPLAY_H
 
+
+
 /**
 * Initializes display manager
 */
 void displayManagerInit(void);
+
 
 /**
 * Waits that display manager terminates
