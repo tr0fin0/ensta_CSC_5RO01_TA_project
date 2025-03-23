b1627168815af46e4ca000a381745c636079751e
Guilherme Nunes Trofino <guitrofino@gmail.com>
2025-03-23 17:08:04 +0100

    [+] messageAdder.c implement algorithm

--- src/messageAdder.c
+++ src/messageAdder.c
@@ -10,57 +10,93 @@
 #include "iAcquisitionManager.h"
 #include "debug.h"
 
-//consumer thread
+
+
+volatile unsigned int consume_count = 0;
+
 pthread_t consumer;
-//Message computed
-volatile MSG_BLOCK out;
-//Consumer count storage
-volatile unsigned int consumeCount = 0;
+
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+
+volatile MSG_BLOCK message_output;
+
+
 
 /**
  * Increments the consume count.
  */
 static void incrementConsumeCount(void);
 
+
 /**
  * Consumer entry point.
  */
-static void *sum( void *parameters );
+static void *sum(void *parameters);
 
 
-MSG_BLOCK getCurrentSum(){
-	//TODO
-}
 
-unsigned int getConsumedCount(){
-	//TODO
-}
-
-
-void messageAdderInit(void){
-	out.checksum = 0;
-	for (size_t i = 0; i < DATA_SIZE; i++)
-	{
-		out.mData[i] = 0;
-	}
-	//TODO
-}
-
-void messageAdderJoin(void){
-	//TODO
-}
-
-static void *sum( void *parameters )
+MSG_BLOCK_ACCUMULATED getCurrentSum()
 {
-	D(printf("[messageAdder]Thread created for sum with id %d\n", gettid()));
+	pthread_mutex_lock(&mutex);
+	MSG_BLOCK current_count = message_output;
+	unsigned int accumulated_count = getConsumedCount();
+
+	MSG_BLOCK_ACCUMULATED current_count_accumulated = {current_count, accumulated_count};
+	pthread_mutex_unlock(&mutex);
+
+    return current_count_accumulated;
+}
+
+
+static void incrementConsumeCount(void)
+{
+	consume_count++;
+}
+
+
+unsigned int getConsumedCount()
+{
+	return (unsigned int) consume_count;
+}
+
+
+void messageAdderInit(void)
+{
+	message_output.checksum = 0;
+	for (size_t i = 0; i < DATA_SIZE; i++)
+		message_output.mData[i] = 0;
+
+    pthread_create(&consumer, NULL, sum, NULL);
+}
+
+
+void messageAdderJoin(void)
+{
+	pthread_join(consumer, NULL);
+}
+
+
+static void *sum(void *parameters)
+{
+	D(printf("[messageAdder] Thread created for sum with id %d\n", gettid()));
 	unsigned int i = 0;
-	while(i<ADDER_LOOP_LIMIT){
+
+	while(i < ADDER_LOOP_LIMIT)
+    {
 		i++;
 		sleep(ADDER_SLEEP_TIME);
-		//TODO
+
+        MSG_BLOCK message_new = getMessage();
+		if (messageCheck(&message_new) == 0)
+			printf("[messageAdder] Message corrupted\n");
+
+		pthread_mutex_lock(&mutex);
+		messageAdd(&message_output, &message_new);
+		pthread_mutex_unlock(&mutex);
+
+        incrementConsumeCount();
 	}
 	printf("[messageAdder] %d termination\n", gettid());
-	//TODO
+
+    pthread_exit(NULL);
 }
-
-
