ace6e8c423d0283e5e0fdb339f6e311e640054ca
Guilherme Nunes Trofino <guitrofino@gmail.com>
2025-03-23 17:07:23 +0100

    [*] iSensor.h format file

--- src/iSensor.h
+++ src/iSensor.h
@@ -3,11 +3,13 @@
 
 #include "msg.h"
 
+
+
 /**
 * Gets the input message.
 * @param input the input number
-* @param mBlock the message pointer returned
+* @param message_block the message pointer returned
 */
-void getInput(const unsigned int input, volatile MSG_BLOCK* mBlock);
+void getInput(const unsigned int input, volatile MSG_BLOCK* message_block);
 
 #endif
