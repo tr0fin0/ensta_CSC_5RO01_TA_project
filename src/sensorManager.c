70fa9a9bd9f1a45e8f295b5f7d5ae324ec709958
Guilherme Nunes Trofino <guitrofino@gmail.com>
2025-03-23 17:11:23 +0100

    [*] sensorManager.c format file

--- src/sensorManager.c
+++ src/sensorManager.c
@@ -4,14 +4,19 @@
 #include "debug.h"
 
 
-void getInput(const unsigned int input, volatile MSG_BLOCK* mBlock){
+
+void getInput(const unsigned int input, volatile MSG_BLOCK* mBlock)
+{
 	unsigned int i;
 	unsigned int j  = input;
 	D(printf("Get message for input %u \n",j));
 	mBlock->checksum = 0;
-	for(i=0;i < DATA_SIZE;i++){
+
+	for(i=0;i < DATA_SIZE;i++)
+    {
 		mBlock->mData[i] = rand();
 		mBlock->checksum ^= mBlock->mData[i];
 	}
+
 	return;
 }
