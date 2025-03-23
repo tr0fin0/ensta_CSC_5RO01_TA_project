666f7f3f423c0d9e9db04ef391fb60c782ac7750
Guilherme Nunes Trofino <guitrofino@gmail.com>
2025-03-23 17:04:50 +0100

    [*] debug.c format file

--- src/display.c
+++ src/display.c
@@ -3,16 +3,21 @@
 #include <stdio.h>
 #include "debug.h"
 
-void messageDisplay(volatile MSG_BLOCK* mBlock){
-	unsigned int i; 
-	messageCheck(mBlock);
+
+
+void messageDisplay(volatile MSG_BLOCK* message_block)
+{
+	unsigned int i;
+	messageCheck(message_block);
+
 	printf("Message\n");
 	D(printf("["));
 	for(i=0;i < DATA_SIZE;i++)
-		D(printf("%u ",mBlock->mData[i]));
+		D(printf("%u ",message_block->mData[i]));
 	D(printf("]\n"));
 }
 
-void print(unsigned int producedCount, unsigned int consumedCount){
-	printf("[displayManager]Produced messages: %d, Consumed messages: %d, Messages left: %d\n", producedCount, consumedCount, producedCount - consumedCount);
+void print(unsigned int producedCount, unsigned int consumedCount)
+{
+	printf("[displayManager] Produced messages: %d, Consumed messages: %d, Messages left: %d\n", producedCount, consumedCount, producedCount - consumedCount);
 }
